ad722f4ab9ee3e85fc99c6642ac900c708a5449e
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-04-01 01:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 function takeANumber(line, newPerson){
   
   line.push(newPerson)
-  return line.length + 1 
+  return `Welcome, ${newPerson}. You are number ${line.length + 1} in line.`
 
 
 }
