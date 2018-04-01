c3fb111c28bd0ab273c0ff4a5d1ff7b5e89d6bcc
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-04-01 02:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ function takeANumber(line, newPerson){
   line.push(newPerson)
   return `Welcome, ${newPerson}. You are number ${line.length + 1} in line.`
 
-
 }
 
+function nowServing(line){
+  
+  line.splice
+  
+}
+
+
+function currentLine(line){
+  
+  string = "The line is currently:"
+  
+  for (var i = 1; i < line.length; i++){
+    
+    string = string + ` ${i+1}. ${line[i]}`
+    if !(i === line.length - 1){
+      string = string + ","
+    }
+  }
+  
+}
