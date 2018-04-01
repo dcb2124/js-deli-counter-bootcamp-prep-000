2ff18276b6f094ef99d107e9c78ae25f530b8304
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-04-01 02:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 function takeANumber(line, newPerson){
   
   line.push(newPerson)
-  return `Welcome, ${newPerson}. You are number ${line.length + 1} in line.`
+  return `Welcome, ${newPerson}. You are number ${line.length} in line.`
 
 }
 
@@ -20,7 +20,7 @@ function currentLine(line){
   var status = "The line is currently empty."
  
  //https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
- //=== won't work between arrays, you have to compare elements or use another library.
+ //=== won't work between arrays, you have to compare elements or use another library. I could also loop through to check 
    if (line.length > 0){
      
     status = "The line is currently:"
